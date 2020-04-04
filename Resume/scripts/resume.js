bb6d610bb9e48652ed2e10f8e34d9534453a57ca
Silas Lowe <60553895+silaslowe@users.noreply.github.com>
2020-04-04 14:37:42 -0500

    Add files via upload
    This is the index.html, js , css and html pages to my resume site.

---
+++ Resume/scripts/resume.js
@@ -0,0 +1,19 @@
+$(function(){
+
+  $("#silas-img").hover(function(){
+      $("#silas-img").animate({
+        height: 200px,
+        width: 200px;}, fast);
+      })
+  })
+
+
+
+    //   $(".card-body").hover(function() {
+//     $(".card-body").animate({
+//         height: 200px,
+//         width: 200px;
+//     },fast);
+
+// $(".card").hover(function(){
+//     $(".card").animate({width: "300px"});
