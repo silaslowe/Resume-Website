2569edfe83fa9a93fd60fa99278585196ff3fe57
Silas Lowe <60553895+silaslowe@users.noreply.github.com>
2020-05-04 21:05:55 -0500

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+var favQuotes = [
+{ quote: 'She said, "Carl, take all the money."/n She called everybody Carl.', author: '-John Prine, Come Back To Us Barbara Lewis Hare Krishna Beauregard'},
+{ quote: 'And you are certainly not the pine-scented air /n There is no way you are the pine-scented air.', author: '-Billy Collins, Litany'}
+]
+
+
+    //   $(".card-body").hover(function() {
+//     $(".card-body").animate({
+//         height: 200px,
+//         width: 200px;
+//     },fast);
+
+// $(".card").hover(function(){
+//     $(".card").animate({width: "300px"});
+
+  // $("#silas-img").hover(function(){
+  //     $("#silas-img").animate({
+  //       height: 200px,
+  //       width: 200px;}, fast);
+  //     })
+  // })
+}
